df760adee3fb9d9c73035dff60e072123972a193
putrachajar <putra.chajar7@gmail.com>
2024-12-04 08:40:37 +0700

    bismillah

--- application/views/admin/Home_view.php
+++ application/views/admin/Home_view.php
@@ -15,91 +15,73 @@
 
 		<div class="container-fluid">
 
-        <!-- 
-        karena ini halaman overview (home), kita matikan partial breadcrumb.
-        Jika anda ingin mengampilkan breadcrumb di halaman overview,
-        silahkan hilangkan komentar (//) di tag PHP di bawah.
-        -->
-		<?php //$this->load->view("admin/_partials/breadcrumb.php") ?>
-
-		<!-- Icon Cards-->
-		<div class="row">
-			<div class="col-xl-3 col-sm-6 mb-3">
-			<div class="card text-white bg-primary o-hidden h-100">
-				<div class="card-body">
-				<div class="card-body-icon">
-					<i class="fas fa-fw fa-comments"></i>
+			<!-- Icon Cards-->
+			<div class="row">
+				<div class="col-xl-3 col-sm-6 mb-3">
+				<div class="card text-white bg-primary o-hidden h-100">
+					<div class="card-body">
+					<div class="card-body-icon">
+						<i class="fas fa-fw fa-desktop"></i>
+					</div>
+					<div class="mr-5">0</div>
+					</div>
+					<a class="card-footer text-white clearfix small z-1" href="javascript:void(0);">
+					<span class="float-left">Komputer Terbanyak Digunakan</span>
+					<span class="float-right">
+						<i class="fas fa-angle-right"></i>
+					</span>
+					</a>
 				</div>
-				<div class="mr-5">26 New Messages!</div>
 				</div>
-				<a class="card-footer text-white clearfix small z-1" href="#">
-				<span class="float-left">View Details</span>
-				<span class="float-right">
-					<i class="fas fa-angle-right"></i>
-				</span>
-				</a>
-			</div>
-			</div>
-			<div class="col-xl-3 col-sm-6 mb-3">
-			<div class="card text-white bg-warning o-hidden h-100">
-				<div class="card-body">
-				<div class="card-body-icon">
-					<i class="fas fa-fw fa-list"></i>
+				<div class="col-xl-3 col-sm-6 mb-3">
+				<div class="card text-white bg-warning o-hidden h-100">
+					<div class="card-body">
+					<div class="card-body-icon">
+						<i class="fas fa-mouse-pointer"></i>
+					</div>
+					<div class="mr-5">0</div>
+					</div>
+					<a class="card-footer text-white clearfix small z-1" href="javascript:void(0);">
+					<span class="float-left">Mouse Terbanyak Digunakan</span>
+					<span class="float-right">
+						<i class="fas fa-angle-right"></i>
+					</span>
+					</a>
 				</div>
-				<div class="mr-5">11 New Tasks!</div>
 				</div>
-				<a class="card-footer text-white clearfix small z-1" href="#">
-				<span class="float-left">View Details</span>
-				<span class="float-right">
-					<i class="fas fa-angle-right"></i>
-				</span>
-				</a>
-			</div>
-			</div>
-			<div class="col-xl-3 col-sm-6 mb-3">
-			<div class="card text-white bg-success o-hidden h-100">
-				<div class="card-body">
-				<div class="card-body-icon">
-					<i class="fas fa-fw fa-shopping-cart"></i>
+				<div class="col-xl-3 col-sm-6 mb-3">
+				<div class="card text-white bg-success o-hidden h-100">
+					<div class="card-body">
+					<div class="card-body-icon">
+						<i class="fas fa-fw fa-keyboard"></i>
+					</div>
+					<div class="mr-5">0</div>
+					</div>
+					<a class="card-footer text-white clearfix small z-1" href="javascript:void(0);">
+					<span class="float-left">Keyboard Terbanyak Digunakan</span>
+					<span class="float-right">
+						<i class="fas fa-angle-right"></i>
+					</span>
+					</a>
 				</div>
-				<div class="mr-5">123 New Orders!</div>
 				</div>
-				<a class="card-footer text-white clearfix small z-1" href="#">
-				<span class="float-left">View Details</span>
-				<span class="float-right">
-					<i class="fas fa-angle-right"></i>
-				</span>
-				</a>
-			</div>
-			</div>
-			<div class="col-xl-3 col-sm-6 mb-3">
-			<div class="card text-white bg-danger o-hidden h-100">
-				<div class="card-body">
-				<div class="card-body-icon">
-					<i class="fas fa-fw fa-life-ring"></i>
+				<div class="col-xl-3 col-sm-6 mb-3">
+				<div class="card text-white bg-danger o-hidden h-100">
+					<div class="card-body">
+					<div class="card-body-icon">
+						<i class="fas fa-fw fa-print"></i>
+					</div>
+					<div class="mr-5">0</div>
+					</div>
+					<a class="card-footer text-white clearfix small z-1" href="javascript:void(0);">
+					<span class="float-left">Printer Terbanyak Digunakan</span>
+					<span class="float-right">
+						<i class="fas fa-angle-right"></i>
+					</span>
+					</a>
 				</div>
-				<div class="mr-5">13 New Tickets!</div>
 				</div>
-				<a class="card-footer text-white clearfix small z-1" href="#">
-				<span class="float-left">View Details</span>
-				<span class="float-right">
-					<i class="fas fa-angle-right"></i>
-				</span>
-				</a>
 			</div>
-			</div>
-		</div>
-
-		<!-- Area Chart Example-->
-		<div class="card mb-3">
-			<div class="card-header">
-			<i class="fas fa-chart-area"></i>
-			Visitor Stats</div>
-			<div class="card-body">
-			<canvas id="myAreaChart" width="100%" height="30"></canvas>
-			</div>
-			<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-		</div>
 
 		</div>
 		<!-- /.container-fluid -->
@@ -111,7 +93,7 @@
 	<!-- /.content-wrapper -->
 
 </div>
-<!-- /#wrapper -->
+<!-- /wrapper -->
 
 
 <?php $this->load->view("admin/_partials/scrolltop.php") ?>
